c3075cb839aa41942e1a5fd038e603553e3e94b2
Marcus <mueller.marcus.go@gmail.com>
2022-06-17 15:51:32 +0200

    added files via upload

---
+++ src/scraping.py
@@ -0,0 +1,187 @@
+from pandas import DataFrame
+import pandas as pd     
+import sys, os
+
+sys.path.append(os.path.join(os.path.dirname(''), '../..'))
+from proj3_gans_scooters.src.utils import install_pip_pkg
+
+### CITY 
+def scrape_wiki_cities() -> DataFrame:
+    from bs4 import BeautifulSoup
+    import requests
+
+    #get html code
+    doc_url = 'https://en.wikipedia.org/wiki/List_of_cities_in_the_European_Union_by_population_within_city_limits'
+    response = requests.get(doc_url)
+    if response.status_code != 200:
+        raise Exception(f'wikipedia returned code {response.status_code} for url = {doc_url}')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.select('table.wikitable > tbody > tr')
+
+    # prettify the names and take only selected ones
+    header = [h.text.strip().replace(' ', '_').lower() for h in table[0].select('th')][1:-2]
+    cities = [[cell.text.strip() for cell in city.select('td')[1:-2]] for city in table[1:]]
+    
+    city_url = 'https://en.wikipedia.org'
+    links = [city_url + city.select('td:nth-child(2) a:first-of-type')[0]['href'] for city in table[1:]]
+
+    return cleanup_cities(add_lat_lon(pd.DataFrame(data=cities, columns=header), links))
+
+def add_lat_lon(df : DataFrame, links : list) -> DataFrame:
+    from bs4 import BeautifulSoup
+    import requests
+    
+    latitude = []
+    longitude = []
+    for city_url in links:
+        city_response = requests.get(city_url)
+        soup = BeautifulSoup(city_response.content, "html.parser")
+        latitude.append(soup.select(".latitude")[0].get_text())
+        longitude.append(soup.select(".longitude")[0].get_text())
+    return df.assign(lat = latitude, lon = longitude)
+
+def cleanup_cities(df : DataFrame):
+    df.loc[:, 'officialpopulation'] = df['officialpopulation'].str.replace(',', '').astype(int)
+    df.loc[:, 'date'] = pd.to_datetime(df['date'])
+
+    # since all latitudes gathered are on the north half and not too close to the equator,
+    # we can just remove all non-digits and put a floating point after the second digit.
+    # If we would look at cities all over the world, we should proceed as we do for the longitude
+    df.loc[:,'lat'] = df.lat.str.replace('\D','', regex=True)
+    df.loc[:,'lat'] = df.lat.str[:2] + '.' + df.lat.str[2:]
+
+    df.loc[:,'lon'] = df.lon.str.replace('.','')
+    df.loc[:,'lon'] = df.lon.str.replace('°','.')
+    mask = df.lon.str[-1] == 'W','lon'
+    df.loc[mask] = '-' + df.loc[mask]
+    df.loc[:,'lon'] = df.lon.replace('[EW]|″|′', '', regex=True)
+    df.loc[:,'lat'] = pd.to_numeric(df['lat'])
+    df.loc[:,'lon'] = pd.to_numeric(df['lon'])
+    return df
+    
+    
+### WEATHER 
+def scrape_weather(city_lst : list, openweather_key : str) -> DataFrame:
+    weather_arguments = {
+        'q' : '', #city
+        'appid' : openweather_key, #api key
+       # 'cnt' : '3', # number of results
+        'units' : 'metric'
+    }
+    
+    weather_lst = []
+    for c in city_lst: 
+        weather_arguments['q'] = c
+        weather_json = get_weather(weather_arguments)
+
+        keep_cols = ['city', 'dt_txt', 'main:temp', 'main:feels_like', 'main:humidity', 
+                     'weather:0:description', 'visibility', 'wind:speed', 'wind:deg', 
+                     'wind:gust', 'pop', 'rain:3h', 'snow:3h', 'sys:pod']
+        weather_df = weather_json_to_df(weather_json, c, keep_cols)
+
+
+        new_cols = ['city', 'date', 'temp_celcius', 'temp_feels_like_celcius', 'humidity_percent', 
+                     'weather_description', 'visibility', 'wind_speed_meter_sec', 'wind_direction_degree', 
+                     'wind_gust_meter_sec', 'pop_percent', 'rain_3h_mm', 'snow_3h_mm', 'pod']
+        weather_df = weather_df.rename(columns=dict(zip(keep_cols, new_cols)))
+        cleanup_weather(weather_df)
+        weather_lst.append(weather_df)
+    df = None
+    if weather_lst:
+        import pandas as pd
+        df = pd.concat(weather_lst, ignore_index=True)
+    return cleanup_weather(df)
+
+def get_weather(weather_arguments : dict) -> dict:
+    import requests
+    import json
+    #check that only string arguments are present
+    if not all(isinstance(val, str) for val in weather_arguments.values()):
+        raise Exception('all arguments must be string')
+        
+    #preparing the request url
+    weather_api = "http://api.openweathermap.org/data/2.5/forecast?"
+    api_arguments = repr(weather_arguments).replace("': '", '=').replace("', '", '&')[2:-2]
+    weather_request = weather_api + api_arguments
+    
+    response = requests.get(weather_request)
+    if response.status_code != 200:
+        raise Exception(f'openweather returned code {response.status_code} for url = {weather_request}')
+    return response.json()
+
+def weather_json_to_df(weather_json : dict, city : str, keep_cols : list=None) -> DataFrame:
+    # install flatdict; needed for weather_json_to_df()
+    #install_pip_pkg({'flatdict'})
+    
+    from flatdict import FlatterDict as flatten
+    from pandas import json_normalize as pd_json_normalize
+    
+    #take weather data and city name
+    weather_df = pd_json_normalize([dict(flatten(i)) for i in weather_json['list']])
+    weather_df = weather_df.assign(city = [city]*weather_df.shape[0])
+    
+    # return only selection of columns
+    if not keep_cols:
+        return weather_df
+    keep_cols = [c for c in keep_cols if c in weather_df.columns]
+    return weather_df[keep_cols]
+
+def cleanup_weather(df : DataFrame):
+    import pandas as pd
+    if 'rain_3h_mm' in df:
+        df.loc[:, 'rain_3h_mm'] = df['rain_3h_mm'].fillna(0)
+    if 'snow_3h_mm' in df:
+        df.loc[:, 'snow_3h_mm'] = df['snow_3h_mm'].fillna(0)
+    df.loc[:, 'date'] = pd.to_datetime(df['date'])
+    return df
+    
+
+### AIRPORTS
+def get_icao_args() -> list:
+    querystring = {"withFlightInfoOnly":"true"}
+    headers = {
+      "X-RapidAPI-Host": "aerodatabox.p.rapidapi.com",
+      "X-RapidAPI-Key": "----needs key-----"
+    }
+    return [querystring, headers]
+
+def icao_airport_codes(
+    city_val : str,
+    latitudes : list, 
+    longitudes : list, 
+    aerodatabox_key : str,
+    args : list = get_icao_args()) -> DataFrame:
+    
+    from bs4 import BeautifulSoup
+    import requests
+    from pandas import json_normalize as pd_json_normalize
+    from pandas import concat as pd_concat
+    
+    assert len(latitudes) == len(longitudes)
+    querystring, headers = args
+    headers['X-RapidAPI-Key'] = aerodatabox_key
+    
+    list_of_df = []
+    for i in range(len(latitudes)):
+        url = f"https://aerodatabox.p.rapidapi.com/airports/search/location/{latitudes[i]}/{longitudes[i]}/km/100/16"
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        if response.status_code != 200:
+            raise Exception(f'aerodatabox returned code {response.status_code} for url = {url}')
+        list_of_df.append(pd_json_normalize(response.json()['items']).assign(city = city_val[i]))
+    
+    if not list_of_df: # empty list returns False
+        raise Exception(f'no airports returned from aerodatabox') 
+
+    return pd_concat(list_of_df, ignore_index=True)
+
+def city_airport_distance(cities_df : DataFrame, airports_df : DataFrame) -> DataFrame:
+    install_pip_pkg({'geopy'})
+    from geopy.distance import distance as geodistance
+                    
+    keep_cols = ['city', 'icao', 'lat_airport', 'lon_airport', 'lat_city', 'lon_city']
+    airport_distance = airports_df.merge(cities_df, on = 'city', suffixes=('_airport', '_city'))[keep_cols]
+    airport_distance['distance_in_km'] = airport_distance.apply(
+        lambda x: geodistance((x.lat_city, x.lon_city), (x.lat_airport, x.lon_airport)).km, axis=1)
+    airport_distance.loc[:,'distance_in_km'] = airport_distance['distance_in_km'].round(2)
+    return airport_distance[['city', 'icao', 'distance_in_km']]
---
+++ src/utils.py
@@ -0,0 +1,213 @@
+def install_pip_pkg(required : set):
+    import sys
+    import subprocess
+    import pkg_resources
+
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
+    
+    if missing:
+        print(f'Trying to install {missing}')
+        python = sys.executable
+        output = None
+        try:
+            result = subprocess.check_call(
+                [python, '-m', 'pip', 'install', *missing], 
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            print(result.returncode, result.stdout, result.stderr)
+            raise subprocess.CalledProcessError(e.output)
+    
+        
+        print(f'Installation successful')
+
+def load_or_execute_df(relative_path, func, args = None):
+    import os
+    from pandas import read_csv as pd_read_csv
+    df = None
+    if os.path.isfile(relative_path):
+        df = pd_read_csv(relative_path, index_col=0)
+    else:
+        if args:
+            df = func(**args)
+        else:
+            df = func()
+        df.to_csv(relative_path)
+    return df
+        
+class PrivateKeysHandler:
+    def __init__(self, relative_path_to_file : str):
+        from configparser import RawConfigParser
+        import os
+        self.configParser = RawConfigParser()   
+        configFilePath = os.path.join(os.path.dirname(''), relative_path_to_file) 
+        self.configParser.read(configFilePath)
+
+    def load_keys(self, section : str) -> dict:
+        return self.configParser[section]
+    
+class MyMySQLConnection:
+    from pandas import DataFrame
+    from sqlalchemy.engine.base import Connection
+    def __init__(
+            self, 
+            credentials : dict, 
+            db_name : str = 'gans_scooters'
+            ):
+                
+        user = credentials['user']
+        password = credentials['password']
+        port = int(credentials['port'])
+        host = credentials['hostname']
+        dialect = credentials.get('dialect', 'mysql')
+        driver = credentials.get('driver', 'pymysql')
+        self.db_name = db_name
+        
+        # setup SQLAlchemy   
+        from sqlalchemy import create_engine 
+        if driver == 'pymysql': install_pip_pkg({'pymysql'})
+        
+        cnx = f'{dialect}+{driver}://{user}:{password}@{host}:{port}/' #{db_name}'
+        
+        # create database if not already created
+        self.alch_engine = create_engine(
+            cnx, 
+            connect_args={'connect_timeout': 10}, 
+            echo=False
+        ) 
+        with self.alch_engine.begin() as con:
+            cursor = con.execute("SHOW DATABASES")
+            if (self.db_name,) not in list(cursor):
+                con.execute(f"CREATE DATABASE {self.db_name}")
+            con.execute(f"USE {self.db_name}")
+                
+    # tables = {table1 : ([columns], [args per column]),
+    #           table2 : ([....
+    # }
+    # i.e
+    # tables = {'Customers' : (['ID', 'Name',...], ['int NOT NULL AUTO_INCREMENT', 'varchar(255) NOT NULL']),
+    #   ...}
+    def create_tables(self, tables : dict):        
+        with self.alch_engine.begin() as con:
+            for table, (columns, args) in tables.items():
+                
+                #drop old table without checking for foreign keys (would throw error)
+                con.execute('SET FOREIGN_KEY_CHECKS = 0')
+                con.execute(f"DROP TABLE IF EXISTS {table}")
+                con.execute('SET FOREIGN_KEY_CHECKS = 1')
+                
+                cols = ',\n'.join(map(lambda x: x[0] + ' ' + x[1], zip(columns, args)))
+                con.execute(f"CREATE TABLE {table} ({cols});")
+                 
+    def execute(self, query : str):
+        with self.alch_engine.begin() as con:
+            con.execute(f'USE {self.db_name};')
+            return con.execute(query)
+        
+        
+    def get_connection(self):
+        return self.alch_engine.connect()
+                    
+    def add_table_to_db(
+            self, 
+            df : DataFrame, 
+            tablename : str, 
+            insert_mode : str): 
+             
+        with self.alch_engine.begin() as con:
+            # replace messes up the column types
+            # instead emptying the table and then appending
+            # keeps all the constraints
+            if insert_mode == 'replace':
+                con.execute(f"TRUNCATE TABLE {tablename}")
+                insert_mode = 'append'
+
+            df.to_sql(
+                #schema="dbo"
+                name= tablename,
+                if_exists=insert_mode, #'replace'
+                con=con, 
+                schema=self.db_name,
+                index=False,
+                chunksize=1000
+            )           
+                
+    def add_tables_to_db(
+            self, 
+            dfs : list, 
+            tablenames : list, 
+            insert_modes : list
+            ):
+                    
+        assert(len(dfs) == len(tablenames))
+        assert(len(dfs) == len(insert_modes))
+        
+        # 'begin' opens a transaction and the 'with' environment cares for a rollback if something 
+        # goes wrong
+        with self.alch_engine.begin() as con:
+            remaining_calls = len(dfs)
+            for i, df in enumerate(dfs):
+                self.add_table_to_db(
+                    df, 
+                    tablenames[i], 
+                    insert_modes[i]
+                )
+
+    #extracts foreign keys into df before adding to db
+    def add_to_db_with_foreign_key(
+            self, 
+            df : DataFrame,
+            tablename : str,
+            foreigntables : list, # list of all tables with a foreign key that need to be extracted
+            foreigncolumns : list, # list of lists: naming the columns to extract for each table
+            matchcolumns : list, # list of lists: naming the columns to merge on for each table; 
+                                 # needs to be the same name in the foreign table and df 
+            insert_mode : str,                                      
+            keepmatchcolumns : list = None): # list of lists: bool if matchcolumns in df should be kept; 
+                                        # does not keep by default; if a column needs to be used several times
+                                        # only pass False for the last occurence
+             
+        # check for correct type and equal lengths
+        assert(len(foreigntables) == len(foreigncolumns))
+        assert(len(foreigncolumns) == len(matchcolumns))
+        for i in range(len(foreigncolumns)):
+            if not isinstance(foreigncolumns[i], list):
+                foreigncolumns[i] = list(foreigncolumns[i])
+            if not isinstance(matchcolumns[i], list):
+                matchcolumns[i] = list(matchcolumns[i])
+            assert(len(foreigncolumns[i]) == len(matchcolumns[i]))
+        
+        if not keepmatchcolumns: # enters if None 
+            from copy import deepcopy
+            keepmatchcolumns = deepcopy(matchcolumns)
+            for i in range(len(matchcolumns)):
+                for j in range(len(matchcolumns[i])):
+                    keepmatchcolumns[i][j] = False
+        else:
+            assert(len(keepmatchcolumns) == len(matchcolumns))
+            for i in matchcolumns:
+                assert(len(keepmatchcolumns[i]) == len(matchcolumns[i]))
+                
+        # retrieve foreign key and send merged table to db
+        with self.alch_engine.begin() as con:
+            from pandas import read_sql as pd_read_sql
+            from pandas import set_option as pd_set_option
+            
+            for i, foreigntable in enumerate(foreigntables):
+                
+                #retrieve foreign key
+                tmp_df = pd_read_sql(f"SELECT {','.join(foreigncolumns[i])}, {','.join(matchcolumns[i])} FROM {foreigntable};", con)
+                for j in range(len(foreigncolumns[i])):
+                    
+                    # suppress "A value is trying to be set on a copy of a slice from a DataFrame." warning
+                    pd_set_option('mode.chained_assignment', None)
+                    df.loc[:,foreigncolumns[i][j]] = df.merge(tmp_df, on=matchcolumns[i][j])[foreigncolumns[i][j]].copy()
+                    pd_set_option('mode.chained_assignment', 'warn')
+                    
+                    # delete column that was used to match if it is not supposed to be inserted into the table
+                    if not keepmatchcolumns[i][j]:
+                        df = df.drop(columns=matchcolumns[i][j])
+                        
+            # send data to sql
+            self.add_table_to_db(df, tablename, insert_mode)
